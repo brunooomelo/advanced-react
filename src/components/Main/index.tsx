5cbca16a2ab8bc6954b3337cce05ffe594751239
brunooomelo <utibmelo@gmail.com>
2020-09-06 10:57:20 -0300

    completed boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-export default function Main() {
+import * as S from './styles'
+
+export default function Main({
+  title = 'React Avançado',
+  description = ' Typescript, ReactJS, NextJS e Styled Components'
+}) {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado"
+      />
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvoldedor de frente para uma tela com código"
+      />
+    </S.Wrapper>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,10 +9,11 @@ function App({ Component, pageProps }: AppProps) {
         <title>Boilerplate</title>
         <link rel="shortcut icon" href="/vercel.svg" />
         <link rel="apple-touch-icon" href="/vercel.svg" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with Typescript,
-         React, NextJS and StyledComponents"
+         React, NextJS and Styled Components"
         />
       </Head>
       <GlobalStyles />
